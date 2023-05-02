b6cd2d1981df5d83aaf0211a3d626146878c58df
gpmatheus <gp.matheusluis@gmail.com>
2023-05-02 00:27:01 -0300

    added springboot backend project

---
+++ WowCollections/src/main/java/com/sonego/WowCollections/WowCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.sonego.WowCollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowCollectionsApplication.class, args);
+	}
+
+}
---
+++ WowCollections/src/test/java/com/sonego/WowCollections/WowCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonego.WowCollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
